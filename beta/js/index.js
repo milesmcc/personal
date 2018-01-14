2a1d1f532c8e6f8dbc1f9f908e8ebfedf9c9b17b
Miles McCain <milesmcc@users.noreply.github.com>
2018-01-14 15:23:54 -0500

    fix double sliding

--- beta/js/index.js
+++ beta/js/index.js
@@ -1,11 +1,16 @@
 particlesJS.load("page", "js/particles.json");
 
-function showContent(id){
-  $(".expand-button").css({'color': "#eee"});
-  $(".expand-button small").removeClass("blink");
-  $(".expand").clearQueue().slideUp(1500, function(){
-    $(id).slideDown(2000);
+function showContent(id) {
+  $(".expand-button").css({
+    'color': "#eee"
   });
-  $(id + "-button").css({'color': "#aa00ff"});
+  $(".expand-button small").removeClass("blink");
+  $(".expand").slideUp(1500);
+  setTimeout(
+    function() {}, 1500);
+  $(id).clearQueue().slideDown(2000);
+  $(id + "-button").css({
+    'color': "#aa00ff"
+  });
   $(id + "-button small").addClass("blink");
 }
