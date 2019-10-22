d0d391d97c3faab7541b7f351486032d9f99b9ce
R. Miles McCain <x1-key@sendmiles.email>
2019-10-22 01:18:06 -0400

    Add footer

---
+++ src/Footer.svelte
@@ -0,0 +1,22 @@
+<style>
+    footer {
+        font-size: 12px;
+        margin-top: 20px;
+    }
+
+    footer a {
+        color: unset;
+    }
+</style>
+
+<div class="columns">
+    <div class="column is-8">
+        <footer class="has-text-grey">
+            &copy; R. Miles McCain {new Date().getFullYear()}. Content is licensed <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA
+                4.0</a>,
+            a Free Culture License. The <a href="https://github.com/milesmcc/personal">source code</a> is available
+            under <a href="https://github.com/milesmcc/personal/blob/master/LICENSE">GPLv3</a>. This site collects (minimal)
+            user analytics.
+        </footer>
+    </div>
+</div>
