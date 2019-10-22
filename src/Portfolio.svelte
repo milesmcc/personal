21ed1fd05440707e1f76d1ca84dc153dd59e9e15
R. Miles McCain <x1-key@sendmiles.email>
2019-10-22 19:46:22 -0400

    Add portfolio header

--- src/Portfolio.svelte
+++ src/Portfolio.svelte
@@ -17,9 +17,16 @@
     <div class="columns">
       <div class="column is-8">
         <p>
-          
+          The work I'm most proud of is <a href="https://privacyspy.org">PrivacySpy</a>,
+          everything by <a href="https://politiwatch.org">Politiwatch</a>
+          (especially <a href="https://polititweet.org">PolitiTweet</a>), and
+          <a href="https://www.foreignaffairs.com/articles/2019-08-02/not-your-fathers-bots">these</a>
+          <a
+            href="https://www.washingtonpost.com/news/monkey-cage/wp/2017/08/24/congress-keeps-quiet-on-u-s-drone-policy-and-thats-a-big-problem/">
+            articles</a>. More granular information about these projects and my other work
+            is available in the portfolio below.
         </p>
-        <MajorSection title="Research" subtitle="Disinformation, oversight, censorship" first=true>
+        <MajorSection title="Research" subtitle="Disinformation, oversight, censorship">
           This is placeholder content.
           <MinorSection title="minor research projects">
             This is where the additional minor research project information would go.
