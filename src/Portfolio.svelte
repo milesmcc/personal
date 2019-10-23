b393a192f78470ceffe940ef2d1eb1b3062362e6
R. Miles McCain <x1-key@sendmiles.email>
2019-10-22 21:16:44 -0400

    Add projects to portfolio

--- src/Portfolio.svelte
+++ src/Portfolio.svelte
@@ -120,9 +120,148 @@
               on my blog, WorthHiding.com</p>
           </MinorSection>
         </MajorSection>
+
         <MajorSection title="Projects" subtitle="PolitiTweet, PrivacySpy, and more">
-          This is placeholder content.
+          <article>
+            <p class="heading is-marginless">Summer 2019 - Present</p>
+            <p><strong>PrivacySpy</strong> &mdash; with <a href="https://igor.fyi">Igor Barakaiev</a> &mdash; <a
+                href="https://privacyspy.org">site</a>, <a href="https://github.com/Politiwatch/PrivacySpy-Core">source
+                code</a> </p>
+            <p>PrivacySpy monitors and rates privacy policies
+              on a ten-point scale, so you can easily understand how your data is being treated online.</p>
+            <p class="has-text-grey">"Privacy policies are long, boring and extremely consequential documents. [...]
+              Which is why I am
+              fascinated by Privacy Spy, a newly launched site that makes privacy policies more convenient and
+              accessible. It’s a little like Wikipedia but for figuring out how a company is using — and misusing — your
+              information." &mdash; <i>Charlie Warzel, writing in the <a
+                  href="https://www.nytimes.com/2019/09/24/opinion/facebook-google-apps-data.html#link-6fea96a5">New
+                  York Times</a></i>
+          </article>
+
+          <article>
+            <p class="heading is-marginless">Fall 2016 - Present</p>
+            <p><strong>PolitiTweet</strong> &mdash; <a href="https://polititweet.org">site</a>, <a
+                href="https://github.com/milesmcc/polititweet">source code</a></p>
+            <p>PolitiTweet preserves the public record by archiving the Tweets of major public figures and prominently
+              displaying those that have been deleted.</p>
+            <p class="has-text-grey">
+              <a
+                href="https://www.nbcnews.com/nightly-news/video/wide-range-of-celebrities-accidentally-tweeted-russian-propaganda-during-2016-election-1087414339671">Nightly
+                News with Lester Holt</a>: "Wide Range of Celebrities Accidentally Tweeted Russian Propaganda During
+              2016 Election" &mdash; "Famous Twitter users with big followers, from Ivanka Trump to comedians Trevor
+              Noah and Sarah Silverman, inadvertently shared tweets that came from accounts linked to Russia,
+              according to social media tracker PolitiTweet."
+            </p>
+            <p class="has-text-grey">
+              <a
+                href="https://www.nbcnews.com/tech/social-media/trump-other-politicians-celebs-shared-boosted-russian-troll-tweets-n817036">NBC
+                News</a>: "Russian Trolls Duped Global Media And Nearly 40 Celebrities" &mdash; "Celebrities and global
+              news outlets retweeted, republished and engaged with tweets by confirmed Kremlin-linked troll accounts
+              thousands of times." (PolitiTweet named as primary source.)
+            </p>
+            <MinorSection title="more of PolitiTweet in the media">
+              <p><a href="http://money.cnn.com/2018/01/19/media/twitter-internet-research-agency-accounts/">CNN</a>:
+                "Twitter to tell 677,775 people they interacted with Kremlin-linked trolls" &mdash; "Twitter will
+                inform nearly 700,000 people in the U.S. that they either followed a Kremlin-linked troll account, or
+                retweeted or liked a tweet sent by one of the accounts, the social media company said Friday."
+                (PolitiTweet linked as source.)</p>
+              <p><a
+                  href="https://www.thedailybeast.com/michael-flynn-followed-russian-troll-accounts-pushed-their-messages-in-days-before-election">The
+                  Daily Beast</a>: "Michael Flynn Followed Russian Troll Accounts, Pushed Their Messages in Days
+                Before Election" &mdash; "Trump&rsquo;s notoriously Kremlin-friendly national security adviser
+                amplified Russian messages right when they mattered most&mdash;in the days leading up to Nov. 8,
+                2016."</p>
+              <p><a href="https://ru.bellingcat.com/novosti/russia/2017/12/03/tengop/">Bellingcat</a>: "Как
+                кремлевский тролль обманул всю Америку" &mdash; Russian investigative journalism piece exposing the
+                propaganda account @TEN_GOP (and cites/is sourced by PolitiTweet). Written by Ben Nimmo and
+                the&nbsp;<a href="https://ru.bellingcat.com/author/dfrlab/">Digital Forensic Research
+                  Lab</a>&nbsp;(DFRLab).</p>
+              <p><a
+                  href="http://www.nydailynews.com/news/national/trump-nicki-minaj-thousand-duped-russian-twitter-bots-article-1.3825400">New
+                  York Daily News</a>: "President Trump, Nicki Minaj, Ann Coulter among hundreds of thousands duped by
+                Russian Twitter bots" &mdash; "Don. Jr has since deleted the February 2017 retweet from @TEN_GOP, but
+                not before nearly 10,000 other social media users shared it, according to PolitiTweet, an online tool
+                published by the nonpartisan, nonprofit tech organization, PolitiWatch."</p>
+            </MinorSection>
+          </article>
+
+          <article>
+            <p class="heading">Winter 2016 - Present</p>
+            <p><strong>WhoAreMyRepresentatives.org</strong> &mdash; <a
+                href="https://whoaremyrepresentatives.org">site</a></p>
+            <p>WhoAreMyRepresentatives.org is a simple tool to look up the government representatives for any
+              U.S. address.</p>
+            <ul class="has-text-grey">
+              <li>Used more than 1 million times and maintains steady traffic.</li>
+              <li>Displayed in the top spot of&nbsp;<a href="https://news.ycombinator.com/item?id=13282847">Hacker
+                  News</a>&nbsp;for over nine hours.</li>
+              <li>Featured on&nbsp;<a href="https://www.producthunt.com/posts/whoaremyrepresentatives">ProductHunt</a>.
+              </li>
+              <li>Hit the front page of&nbsp;<a
+                  href="https://www.reddit.com/r/InternetIsBeautiful/comments/5kyn35/enter_your_address_and_out_everyone_who/">Reddit</a>
+                (5k+ upvotes).
+              </li>
+              <li>Received an (informal) invitation to visit Google.</li>
+              <li>Shared by&nbsp;<a href="https://twitter.com/DaveLeeBBC/status/814865319290425344">Dave
+                  Lee</a>,&nbsp;<a href="https://twitter.com/colinismyname/status/814907573522591744">Colin
+                  Wright</a>,&nbsp;<a href="https://twitter.com/HANAtruly/status/823653522084216832">HANA</a>,&nbsp;<a
+                  href="https://twitter.com/MikeElgan/status/815750507738206208">Mike Elgan</a>, and others.</li>
+              <li>Featured in&nbsp;<a
+                  href="https://boingboing.net/2017/01/03/find-out-who-represents-you-in.html">BoingBoing</a>,&nbsp;<a
+                  href="https://www.bustle.com/p/how-to-call-your-representative-urge-them-to-boycott-the-inauguration-31044">Bustle</a>,&nbsp;<a
+                  href="https://www.timeout.com/usa/blog/check-out-this-site-to-easily-find-out-who-your-representatives-are-and-how-to-contact-them-012517">TimeOut</a>&nbsp;(<a
+                  href="https://www.timeout.com/newyork/blog/15-things-you-can-do-after-the-womens-march-to-stay-engaged-in-nyc-012417">twice</a>),
+                &amp; others.</li>
+              <li>Linked to as an activism resource by Planned Parenthood.</li>
+            </ul>
+          </article>
+          <MinorSection title="additional projects">
+            <ul>
+              <li><a href="https://whyprivacymatters.org/">WhyPrivacyMatters.org</a>&nbsp;&mdash; a collaborative
+                project to argue for privacy; translated into 14 languages (and counting); over 28&nbsp;<a
+                  href="https://whyprivacymatters.org/CONTRIBUTORS.html">contributors</a>.
+              </li>
+              <li><a href="https://github.com/milesmcc/ieql">IEQL</a>&nbsp;&mdash; an Internet scanning system I
+                created as part of an independent project course at Phillips Academy.</li>
+              <li><a href="https://pollpa.org/">PollPA</a>&nbsp;&mdash; a live polling platform for Phillips Academy.
+              </li>
+              <li><a href="https://parussian.org/">PARussian</a>&nbsp;&mdash; a 2nd level textbook I created based on
+                Русский Язык Для Всех for the Phillips Academy Russian Department.</li>
+              <li><a href="https://foia.fyi/">Foia.fyi</a>&nbsp;&mdash; a simple website that simplifies the process
+                of submitting a FOIA request.</li>
+              <li><a href="https://armorlib.org/">ArmorLib</a>&nbsp;&mdash; a malware detection framework I created as
+                part of a course at Phillips Academy.</li>
+              <li><a href="https://concision.co/">Concision.co</a>&nbsp;&mdash; a simple word processing tool for
+                quick copy-paste analysis.</li>
+              <li><a href="https://openwi.re/">OPENWI:RE</a>&nbsp;&mdash; a weekly newsletter highlighting the best of
+                the week&rsquo;s adversarial journalism and &ldquo;think&rdquo; pieces, with an emphasis on issues of
+                foreign policy, digital privacy, and technology. (Discontinued.)</li>
+              <li><a href="https://github.com/milesmcc/deaddrop">Deaddrop</a>&nbsp;&mdash; secure, authenticated, and
+                monitored file 'dead drops.'</li>
+              <li><a href="https://librenews.io/">LibreNews</a>&nbsp;&mdash; a free, open source, speedy, and deeply
+                customizable breaking news notification system for Android phones. Over three thousand active daily
+                users.</li>
+              <li><a href="https://tbij.dronescout.org/">DroneScout API</a>&nbsp;&mdash; an API and web interface for
+                processing the Bureau of Investigative Journalism's drone strike data.</li>
+              <li><a href="https://530zulu.com/">530ZULU</a>&nbsp;&mdash; a twice daily concise email news briefing.
+                (Discontinued.)</li>
+              <li><a href="https://github.com/milesmcc/vog">Vog</a>&nbsp;&mdash; an open source browser extension
+                (Firefox, Opera, and Chrome) for extracting open-graph data from a webpage without having to deal with
+                source files.</li>
+              <li>TrumpWire &mdash; a project (now defunct) that sent a SMS notification whenever Donald Trump signed
+                an executive order.</li>
+              <li><a href="https://github.com/milesmcc/Encompassed.press">Encompassed.press</a>&nbsp;&mdash; the
+                single most encompassing and important news story, found automatically. (Discontinued.)</li>
+              <li><a href="http://labs.md-c.com/">MD-C Labs</a>&nbsp;&mdash; a place for experimental web projects.
+                Not production-ready, and from 3+ years ago.</li>
+              <li><a href="https://bibleorquran.org/">BibleOrQuran.org</a>&nbsp;&mdash; a game to combat islamophobia;
+                awarded "most wholesome [game] of 2016" by Reddit WebGames, 250+ upvotes on&nbsp;<a
+                  href="https://www.reddit.com/r/WebGames/comments/50cwkn/bibleorquran_gives_you_a_random_verse_from_either/">post</a>
+              </li>
+            </ul>
+          </MinorSection>
         </MajorSection>
+
         <MajorSection title="Academics" subtitle="Classes, prizes, organizations">
           This is placeholder content.
         </MajorSection>
