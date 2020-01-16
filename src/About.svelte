da839b098aa063463fa65dc635bd808f34c9f32a
R. Miles McCain <x1-key@sendmiles.email>
2020-01-16 11:19:15 +0000

    Add OSINT :)

--- src/About.svelte
+++ src/About.svelte
@@ -21,9 +21,10 @@
       <div class="column is-8">
         <p>
           I want to build systems that improve the world—and learn something
-          along the way. My interests include internet freedom, cybersecurity,
+          along the way. My interests include digital freedom, cybersecurity,
           government accountability, political science, artificial intelligence,
           and human-centered design.<FootnoteRef {scope} index="1" />
+          <span class="has-text-white">And OSINT.</span>
         </p>
         <p>
           My projects, research, and commentary have appeared in several major
