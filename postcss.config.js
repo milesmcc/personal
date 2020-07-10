92a423f6b12981a7805ecb01e87475921bce2909
R. Miles McCain <development-machine@sendmiles.email>
2020-07-10 03:57:33 +0000

    Add local PostCSS

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    plugins: [
+        require("postcss-preset-env")(),
+        require("postcss-import")(),
+        require("autoprefixer")(),
+        require("@fullhuman/postcss-purgecss")({
+            content: ['**/*.{md,html}'],
+            defaultExtractor: content => {
+                const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+                const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+                return broadMatches.concat(innerMatches)
+            }
+        }),
+        require("cssnano")({ preset: 'default' }),
+    ]
+};
