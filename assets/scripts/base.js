17325ff0f90a7230809a94b7335a3ad07abd1a6f
R. Miles McCain <development-machine@sendmiles.email>
2020-07-10 05:18:48 +0000

    Fix table of contents after purge

--- assets/scripts/base.js
+++ assets/scripts/base.js
@@ -2,11 +2,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
-            const id = entry.target.getAttribute('id');
-            if (entry.intersectionRatio > 0) {
-                document.querySelector(`#TableOfContents a[href="#${id}"]`).classList.add('active');
-            } else {
-                document.querySelector(`#TableOfContents a[href="#${id}"]`).classList.remove('active');
+            const element = document.querySelector(`#TableOfContents a[href="#${entry.target.getAttribute('id')}"]`);
+            if (element !== null) {
+                if (entry.intersectionRatio > 0 && element) {
+                    element.classList.add('active');
+                } else {
+                    element.classList.remove('active');
+                }
             }
         });
     });
