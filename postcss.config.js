46cd0eaa76e4e6f0a9ed3d791704f419e0042929
R. Miles McCain <development-machine@sendmiles.email>
2020-07-10 04:33:45 +0000

    Only purge css in production

--- postcss.config.js
+++ postcss.config.js
@@ -3,14 +3,16 @@ module.exports = {
         require("postcss-preset-env")(),
         require("postcss-import")(),
         require("autoprefixer")(),
-        require("@fullhuman/postcss-purgecss")({
-            content: ['**/*.{md,html}'],
-            defaultExtractor: content => {
-                const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
-                const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
-                return broadMatches.concat(innerMatches)
-            }
-        }),
+        ...process.env.HUGO_ENV === 'production'
+            ? [require("@fullhuman/postcss-purgecss")({
+                content: ['**/*.{md,html}'],
+                defaultExtractor: content => {
+                    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+                    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+                    return broadMatches.concat(innerMatches)
+                }
+            })]
+            : [],
         require("cssnano")({ preset: 'default' }),
     ]
 };
