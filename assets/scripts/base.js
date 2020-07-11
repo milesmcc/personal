76860ed560a94d1b861ab2085abe7164c192a227
R. Miles McCain <development-machine@sendmiles.email>
2020-07-11 20:14:33 +0000

    Add Netlify CMS

--- assets/scripts/base.js
+++ assets/scripts/base.js
@@ -27,4 +27,15 @@ window.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('[id]').forEach((section) => {
         observer.observe(section);
     });
+
+    // Redirect for Netlify identity
+    if (window.netlifyIdentity) {
+        window.netlifyIdentity.on("init", user => {
+            if (!user) {
+                window.netlifyIdentity.on("login", () => {
+                    document.location.href = "/admin/";
+                });
+            }
+        });
+    }
 });
