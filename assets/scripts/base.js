23129fea22b8d36256f39bd3e4e35ddf41fbb7d9
R. Miles McCain <github@sendmiles.email>
2023-04-22 21:41:49 -0700

    Add easter egg

--- assets/scripts/base.js
+++ assets/scripts/base.js
@@ -3,3 +3,18 @@ import {listen} from 'quicklink';
 window.addEventListener('load', () => {
     listen();
 });
+
+// Chaotic function to remove all stylesheets and set background color.
+// Make everything look like a 90s website.
+window.stripStyles = () => {
+    // Remove all stylesheets
+    const stylesheets = [...document.getElementsByTagName('link')];
+
+    for(let sheet of stylesheets) {
+        if(sheet.getAttribute("rel") == "stylesheet")
+            sheet.parentNode.removeChild(sheet);
+    }
+
+    // Set background to #FEFED6 and make other changes to make it look like a 90s website.
+    document.body.style.backgroundColor = '#FEFED6';
+}
